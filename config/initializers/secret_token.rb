b6a63326a821c6d861fd77d5beb2bd21127f56a3
Boris Vanyushin <b.vanyushin@gmail.com>
2015-01-12 17:48:35 +0300

    generate new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cardsharper::Application.config.secret_key_base = '32c94af9d18617c723fd21323d82d63c4bddb93eab7b4c9f4c6a2a018fa761395b584281964bdb164c790a88c044c3d90c3d1b5057a022851591a56e66b4e015'
