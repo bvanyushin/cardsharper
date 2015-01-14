276738aa4b2bb07fbb5681b146794041e2343f73
Boris Vanyushin <b.vanyushin@gmail.com>
2015-01-14 19:44:39 +0300

    Add Card.create

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -9,4 +9,18 @@ class CardsController < ApplicationController
 
   def new
   end
+
+  def create
+    @card = Card.new(card_params)
+
+    @card.save 
+    redirect_to @card
+  end
+
+  private
+    def card_params
+      allow = [:original_text, :translated_text]
+      params.require(:card).permit(allow)
+    end
+
 end
