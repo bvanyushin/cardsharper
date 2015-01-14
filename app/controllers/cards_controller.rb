1584e9e952f66ad4c1d3c60e3941789c698640ed
Boris Vanyushin <b.vanyushin@gmail.com>
2015-01-14 22:37:44 +0300

    Add delete

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -36,6 +36,13 @@ class CardsController < ApplicationController
     end
   end
 
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+
+    redirect_to cards_path
+  end
+
   private
     def card_params
       allow = [:original_text, :translated_text]
