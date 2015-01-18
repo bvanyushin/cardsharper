9e3e916e4cc054618d2f013e85890b6c1a4c6e22
Boris Vanyushin <b.vanyushin@gmail.com>
2015-01-18 04:40:14 +0300

    Style issues fixed

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,10 +5,10 @@ class Card < ActiveRecord::Base
 
   def move_review_date!(days_to_move = 3)
     self.review_date = Time.now.midnight + days_to_move.day
-    self.save
+    save
   end
 
   def answer_correct?(user_answer)
-    self.translated_text == user_answer
+    translated_text == user_answer
   end
 end
