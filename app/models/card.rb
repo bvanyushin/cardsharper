56b4b8ef7be998fb2f1f564243322625744bdf89
Boris Vanyushin <b.vanyushin@gmail.com>
2015-01-18 04:21:26 +0300

    Stupid filtration bug fixed

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,8 +1,7 @@
 class Card < ActiveRecord::Base
   validates :translated_text, :original_text, presence: true
   validates_with CardTextFieldsDifferenceValidator
-
-  scope :actual_on, -> due_date { where("review_date >= ?", due_date) } 
+  scope :actual_on, -> due_date { where("review_date <= ?", due_date) } 
 
   def move_review_date!(days_to_move = 3)
     self.review_date = Time.now.midnight + days_to_move.day
