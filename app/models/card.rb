f9af4aa4e3dd1d2691ab04f587a948da1efbc017
Boris Vanyushin <b.vanyushin@gmail.com>
2015-01-19 01:53:02 +0300

    Continue refactoring

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,10 +1,8 @@
 class Card < ActiveRecord::Base
   validates :translated_text, :original_text, presence: true
   validates_with CardTextFieldsDifferenceValidator
-  
-  after_initialize :set_review_date
-  
-  scope :relevant_for_today, -> { where("review_date <= ?", Time.now).order("RANDOM()")  }
+  after_create :set_review_date
+  scope :relevant_for_today, -> { where("review_date <= ?", Time.now).order("RANDOM()") }
   
   def review(user_answer)
     if translated_text.downcase == user_answer.strip.downcase
@@ -19,6 +17,6 @@ class Card < ActiveRecord::Base
   private
 
   def set_review_date
-    update_attributes(review_date:Time.now.midnight)
+    update_attributes(review_date: Time.now.midnight)
   end
 end
