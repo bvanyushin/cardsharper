19fe9dac6a121278765cef1733c953d114183618
Boris Vanyushin <b.vanyushin@gmail.com>
2015-01-23 15:24:13 +0300

    Add user register page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,16 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+     @user = User.new(user_params)
+
+    if @user.save 
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
   private
 
   def user_params
