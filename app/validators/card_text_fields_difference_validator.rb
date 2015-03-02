63dc1c823fe7e8e51fd8440684ce3475cf775394
Boris Vanyushin <b.vanyushin@gmail.com>
2015-03-02 14:59:33 +0300

    Validation improved

--- app/validators/card_text_fields_difference_validator.rb
+++ app/validators/card_text_fields_difference_validator.rb
@@ -1,6 +1,6 @@
 class CardTextFieldsDifferenceValidator < ActiveModel::Validator
   def validate(card)
-    if card.original_text == card.translated_text
+    if card.original_text.strip.downcase == card.translated_text.strip.downcase
       card.errors[:base] << "Fields must have different values."
     end
   end
