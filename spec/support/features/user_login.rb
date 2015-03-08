3e2660add21aec71859c6ddc39b03b4863fd7885
Boris Vanyushin <b.vanyushin@gmail.com>
2015-03-08 22:15:10 +0300

    Styke issues fixed

--- spec/support/features/user_login.rb
+++ spec/support/features/user_login.rb
@@ -3,7 +3,7 @@ def login_user(email, password)
   click_link("Войти")
   within("#login-form") do
     fill_in "email", with: email
-    fill_in "password", with: "password"
+    fill_in "password", with: password
   end
   click_button "Войти"
 end
