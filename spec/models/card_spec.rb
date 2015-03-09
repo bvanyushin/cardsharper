71041e69fbcc71e2c60dfc39b1cfd70fa88e5b85
Boris Vanyushin <b.vanyushin@gmail.com>
2015-03-09 21:00:27 +0300

    Unit test for review date added

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -16,16 +16,86 @@ describe Card do
     expect(card.review("Incorrect value")).to be false
   end
 
-  it "moves the review date if answer is correct 3 days from today if review date is today" do
-    card.review_date = Date.today
-    card.review("Correct value")
-    expect(card.review_date).to eql Date.today + 3.day
-  end
+  describe "review process" do
+    it "increment attempt counter if answer is correct" do
+      card.attempt_count = 0
+      card.review("Correct value")
+      expect(card.attempt_count).to eql 1
+    end
 
-  it "moves the review date if answer is correct 3 days from today for any date in past" do
-    card.review_date = Date.yesterday
-    card.review("Correct value")
-    expect(
-      card.review_date).to eql Date.today + 3.day
+    it "moves the review date if answer is correct for dates in past" do
+      card.review_date = Date.yesterday
+      card.review("Correct value")
+      expect(card.review_date).not_to eql Date.yesterday
+    end
+
+    it "nullify fail attempts counter if answer is correct" do
+      card.failed_attempt_count = 2
+      card.review("Correct value")
+      expect(card.failed_attempt_count).to eql 0
+    end
+
+    it "increment fail attempts counter if answer is incorrect" do
+      card.failed_attempt_count = 0
+      card.review("Incorrect")
+      expect(card.failed_attempt_count).to eql 1
+    end
+
+    it "nullify fail attempts counter after 3 incorrect answers" do
+      card.failed_attempt_count = 2
+      card.review("Incorrect")
+      expect(card.failed_attempt_count).to eql 0
+    end
+
+    it "nullify attempts counter after 3 incorrect answers" do
+      card.failed_attempt_count = 2
+      card.attempt_count = 3
+      card.review("Incorrect")
+      expect(card.attempt_count).to eql 0
+    end
+
+    it "set review date 12 hours from now for 3 incorrect answers in row" do
+      card.failed_attempt_count = 2
+      card.attempt_count = 3
+      card.review("Incorrect")
+      expect(card.review_date).to be_within(20.seconds).of(Time.now + 12.hours)
+    end
+
+    describe "moves review_date for correct answer for" do
+      it "12 hours from now if this is first review" do
+        card.review_date = Date.today
+        card.attempt_count = 0
+        card.review("Correct value")
+        expect(card.review_date).to be_within(20.seconds).of(Time.now + 12.hours)
+      end
+
+      it "3 days from now if this is second review" do
+        card.review_date = Date.today
+        card.attempt_count = 1
+        card.review("Correct value")
+        expect(card.review_date).to be_within(20.seconds).of(Time.now + 3.days)
+      end
+
+      it "1 week from now if this is third review" do
+        card.review_date = Date.today
+        card.attempt_count = 2
+        card.review("Correct value")
+        expect(card.review_date).to be_within(20.seconds).of(Time.now + 1.week)
+      end
+
+      it "2 weeks from now if this is fourth review" do
+        card.review_date = Date.today
+        card.attempt_count = 3
+        card.review("Correct value")
+        expect(card.review_date).to be_within(20.seconds).of(Time.now + 2.weeks)
+      end
+
+      it "1 month from now if this is fifth review" do
+        card.review_date = Date.today
+        card.attempt_count = 4
+        card.review("Correct value")
+        expect(card.review_date).to be_within(20.seconds).of(Time.now + 1.month)
+      end
+    end
   end
 end
