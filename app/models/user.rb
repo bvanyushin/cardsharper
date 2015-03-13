63612bef9c88a7d327d8cac5fa0d4d27aac8927b
Boris Vanyushin <b.vanyushin@gmail.com>
2015-03-13 15:44:17 +0300

    whenever gem and schedule added, notification method to User model added

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,16 @@ class User < ActiveRecord::Base
   def set_deck(deck_id)
     update_attributes(current_deck_id: deck_id)
   end
+
+  def self.notify_cards
+    mail_list = []
+    users.each do |user|
+      if user.cards.relevant_for_today.first.present
+        mail_list.push(user)
+      end
+    end
+    mail_list.each do |user|
+      NotificationsMailer.pending_cards(user)
+    end
+  end
 end
