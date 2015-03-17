3f188198ffde6f64f24461729bf340e4ac75bd80
Boris Vanyushin <b.vanyushin@gmail.com>
2015-03-17 14:10:31 +0300

    email and url transered to env settings

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,9 +1,9 @@
 class NotificationMailer < ApplicationMailer
-  default from: "notifications@example.com"
+  default from: ENV["CS_NOTIFICATION_FROM_EMAIL"]
 
   def pending_cards(user)
     @user = user
-    @url  = "http://cardsharper.herokuapp.com/"
+    @url  = ENV["CS_URL"]
     @sample_word = user.cards.relevant_for_today.first.original_text
     mail(to: @user.email, subject: "Как переводится #{@sample_word}?")
   end
